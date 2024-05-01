f563bfe0cad126ed0d5af02c08c5315f97cb3cac
keymii <abhi.vansh2017@gmail.com>
2024-05-01 21:14:34 +0530

    1000 iterations

--- RedundantNullChecks.java
+++ RedundantNullChecks.java
@@ -2,31 +2,37 @@ import java.util.Random;
 
 public class RedundantNullChecks {
     public static void main(String[] args) {
-        Random random = new Random();
-        int randomNumber = random.nextInt(20); // Random number between 0 and 19
-        System.out.println(randomNumber);
-        Object obj;
-        if (randomNumber < 10) {
-            obj = null;
-        } else {
-            obj = new Object();
-        }
-        
-        if (obj == null) {
-            System.out.println("Object is null");
-            return;
-        } else {
-            System.out.println("Object is not null");
-        }
-        
-        // Redundant null checks
-        // If obj == null, code would not reach here
-        if (obj != null) {
-            System.out.println("Object is not null");
-        }
+        long startTime = System.nanoTime();
+        for (int i=0; i<1000; i++){
+            Random random = new Random();
+            int randomNumber = random.nextInt(20); // Random number between 0 and 19
+            System.out.println(randomNumber);
+            Object obj;
+            if (randomNumber < 10) {
+                obj = null;
+            } else {
+                obj = new Object();
+            }
+            
+            if (obj == null) {
+                System.out.println("Object is null");
+                continue;
+            } else {
+                System.out.println("Object is not null");
+            }
+            
+            // Redundant null checks
+            // If obj == null, code would not reach here
+            if (obj != null) {
+                System.out.println("Object is not null");
+            }
 
-        if (obj != null) {
-            System.out.println("Object is not null");
+            if (obj != null) {
+                System.out.println("Object is not null");
+            }
         }
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime) / 1000000;
+        System.out.println("Execution time: " + duration + " milliseconds");
     }
 }
